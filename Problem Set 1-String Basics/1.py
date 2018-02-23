1e930891b4a4b20370f9b691def9e33ce5988914
psingh195 <praneet195@gmail.com>
2018-02-23 14:11:43 +0530

    second commit

---
+++ Problem Set 1-String Basics/1.py
@@ -0,0 +1,5 @@
+vow=0
+for letter in s:
+    if letter=='a' or letter=='e' or letter=='i' or letter=='u' or letter=='o':
+        vow+=1
+print("Number of vowels: ",vow)   
---
+++ Problem Set 1-String Basics/3.py
@@ -0,0 +1,16 @@
+r = ''
+c = ''
+for char in s:
+    if (c == ''):
+        c = char
+    elif (c[-1] <= char):
+        c += char
+    elif (c[-1] > char):
+        if (len(r) < len(c)):
+            r = c
+            c = char
+        else:
+            c = char
+if (len(c) > len(r)):
+    r = c
+print(r)
